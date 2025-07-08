5aa62b0585dc23f73edc3f6a44e002a126ac59b0
Rudi Merlos Carrión <rudimerlos@gmail.com>
2025-07-08 20:24:33 +0200

    First commit

---
+++ src/test/java/org/rmc/training_platform/TrainingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rmc.training_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
